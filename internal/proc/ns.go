0a5b9ec8672276e89a44d6efe531900279cfd021
abdfnx <abdfnabd@gmail.com>
2021-08-24 17:11:56 +0300

    build `ReadMappings` function to `internal\proc\ns.go`

--- internal/proc/ns.go
+++ internal/proc/ns.go
@@ -1,8 +1,12 @@
 package proc
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
+
+	"github.com/pkg/errors"
 )
 
 type IDMap struct {
@@ -27,4 +31,38 @@ func ParseUserNamespace(pid string) (string, error) {
 	}
 
 	return userNS, nil
+}
+
+func ReadMappings(path string) ([]IDMap, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot open %s", path)
+	}
+
+	defer file.Close()
+
+	mappings := []IDMap{}
+
+	buf := bufio.NewReader(file)
+	for {
+		line, _, err := buf.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				return mappings, nil
+			}
+
+			return nil, errors.Wrapf(err, "cannot read line from %s", path)
+		}
+
+		if line == nil {
+			return mappings, nil
+		}
+
+		containerID, hostID, size := 0, 0, 0
+		if _, err := fmt.Sscanf(string(line), "%d %d %d", &containerID, &hostID, &size); err != nil {
+			return nil, errors.Wrapf(err, "cannot parse %s", string(line))
+		}
+
+		mappings = append(mappings, IDMap{ContainerID: containerID, HostID: hostID, Size: size})
+	}
 }
