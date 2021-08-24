cdb6835e16d6c15faf49cc21ff0f6c6006f3a737
abdfnx <abdfnabd@gmail.com>
2021-08-24 16:27:55 +0300

    add `majDevNum` & `minDevNum` functions to `internal\dev\tty.go`

--- internal/dev/tty.go
+++ internal/dev/tty.go
@@ -5,3 +5,12 @@ type TTY struct {
 	Major uint64
 	Path string
 }
+
+func majDevNum(rdev uint64) uint64 {
+	return (rdev >> 8) & 0xfff
+}
+
+// minDevNum returns the minor device number of rdev (see stat_t.Rdev).
+func minDevNum(rdev uint64) uint64 {
+	return (rdev & 0xff) | ((rdev >> 12) & 0xfff00)
+}
