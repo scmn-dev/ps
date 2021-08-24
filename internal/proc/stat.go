29907d5744332ec1054c903049e2b7411ddc910d
abdfnx <abdfnabd@gmail.com>
2021-08-24 17:39:30 +0300

    add the requirements again, return `&Stat`

--- internal/proc/stat.go
+++ internal/proc/stat.go
@@ -63,4 +63,30 @@ func ParseStat(pid string) (*Stat, error) {
 	fieldAt := func(i int) string {
 		return fields[i-1]
 	}
+
+	return &Stat{
+		Pid:         fieldAt(1),
+		Comm:        fieldAt(2),
+		State:       fieldAt(3),
+		Ppid:        fieldAt(4),
+		Pgrp:        fieldAt(5),
+		Session:     fieldAt(6),
+		TtyNr:       fieldAt(7),
+		Tpgid:       fieldAt(8),
+		Flags:       fieldAt(9),
+		Minflt:      fieldAt(10),
+		Cminflt:     fieldAt(11),
+		Majflt:      fieldAt(12),
+		Cmajflt:     fieldAt(13),
+		Utime:       fieldAt(14),
+		Stime:       fieldAt(15),
+		Cutime:      fieldAt(16),
+		Cstime:      fieldAt(17),
+		Priority:    fieldAt(18),
+		Nice:        fieldAt(19),
+		NumThreads:  fieldAt(20),
+		Itrealvalue: fieldAt(21),
+		Starttime:   fieldAt(22),
+		Vsize:       fieldAt(23),
+	}, nil
 }
