1bba59623a633ffde64d57979ac58691547225cd
abdfnx <abdfnabd@gmail.com>
2021-08-24 17:07:56 +0300

    update `internal\proc\ns.go` file by adding `ParsePIDNamespace` function

--- internal/proc/ns.go
+++ internal/proc/ns.go
@@ -1,7 +1,21 @@
 package proc
 
+import (
+	"fmt"
+	"os"
+)
+
 type IDMap struct {
 	ContainerID int
 	HostID      int
 	Size        int
+}
+
+func ParsePIDNamespace(pid string) (string, error) {
+	pidNS, err := os.Readlink(fmt.Sprintf("/proc/%s/ns/pid", pid))
+	if err != nil {
+		return "", err
+	}
+
+	return pidNS, nil
 }
