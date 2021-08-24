9ec8373f62084195317e9d159b5b408468b099d5
abdfnx <abdfnabd@gmail.com>
2021-08-24 17:48:46 +0300

    create `Status` struct in `proc` package

---
+++ internal/proc/status.go
@@ -0,0 +1,57 @@
+package proc
+
+type Status struct {
+	Name string
+	Umask string
+	State string
+	Tgid string
+	Ngid string
+	Pid string
+	PPid string
+	TracerPid string
+	Uids []string
+	Gids []string
+	FdSize string
+	Groups []string
+	NStgid string
+	NSpid []string
+	NSpgid string
+	NSsid string
+	VMPeak string
+	VMSize string
+	VMLCK string
+	VMPin string
+	VMHWM string
+	VMRSS string
+	RssAnon string
+	RssFile string
+	RssShmem string
+	VMData string
+	VMStk string
+	VMExe string
+	VMLib string
+	VMPTE string
+	VMPMD string
+	VMSwap string
+	HugetlbPages string
+	Threads string
+	SigQ string
+	SigPnd string
+	ShdPnd string
+	SigBlk string
+	SigIgn string
+	SigCgt string
+	CapInh string
+	CapPrm string
+	CapEff string
+	CapBnd string
+	CapAmb string
+	NoNewPrivs string
+	Seccomp string
+	CpusAllowed string
+	CpusAllowedList string
+	MemsAllowed string
+	MemsAllowedList string
+	VoluntaryCtxtSwitches string
+	NonvoluntaryCtxtSwitches string
+}
