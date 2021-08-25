1d806bb4c15722f9be8a166f121d1d56fe660bde
abdfnx <abdfnabd@gmail.com>
2021-08-25 10:36:12 +0300

    add `psContext` struct and `processFunc` function

--- ps.go
+++ ps.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"github.com/gepis/ps/internal/dev"
+	"github.com/gepis/ps/internal/process"
+)
+
 type IDMap struct {
 	ContainerID int
 	HostID int
@@ -11,3 +16,12 @@ type JoinNamespaceOpts struct {
 	GIDMap []IDMap
 	FillMappings bool
 }
+
+type psContext struct {
+	containersProcesses []*process.Process
+	hostProcesses []*process.Process
+	ttys *[]dev.TTY
+	opts *JoinNamespaceOpts
+}
+
+type processFunc func(*process.Process, *psContext) (string, error)
