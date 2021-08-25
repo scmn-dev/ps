9e945de5c3feeffcc1faa94e0734e5a9de2f0609
abdfnx <abdfnabd@gmail.com>
2021-08-25 10:42:53 +0300

    add `aixFormatDescriptor` & `findID` function

--- ps.go
+++ ps.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+	"io/ioutil"
+
 	"github.com/gepis/ps/internal/dev"
 	"github.com/gepis/ps/internal/process"
+	"github.com/pkg/errors"
 )
 
 type IDMap struct {
@@ -25,3 +30,38 @@ type psContext struct {
 }
 
 type processFunc func(*process.Process, *psContext) (string, error)
+
+type aixFormatDescriptor struct {
+	code string
+	normal string
+	header string
+	onHost bool
+	procFn processFunc
+}
+
+func findID(idStr string, mapping []IDMap, lookupFunc func(uid string) (string, error), overflowFile string) (string, error) {
+	if len(mapping) == 0 {
+		return idStr, nil
+	}
+
+	id, err := strconv.ParseInt(idStr, 10, 0)
+	if err != nil {
+		return "", errors.Wrapf(err, "cannot parse %s", idStr)
+	}
+
+	for _, m := range mapping {
+		if int(id) >= m.ContainerID && int(id) < m.ContainerID+m.Size {
+			user := fmt.Sprintf("%d", m.HostID+(int(id)-m.ContainerID))
+
+			return lookupFunc(user)
+		}
+	}
+
+	// User not found, read the overflow
+	overflow, err := ioutil.ReadFile(overflowFile)
+	if err != nil {
+		return "", errors.Wrapf(err, "cannot read %s", overflowFile)
+	}
+
+	return string(overflow), nil
+}
